a23e25661ed193b7935c29b54d2114542d260308
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-23 10:38:35 -0500

    backend

---
+++ backend/src/test/java/com/sergiorosa/ecatalog/ECatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.ecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
