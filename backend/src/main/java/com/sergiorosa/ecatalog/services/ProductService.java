1b5db00bcc82e4495c340c5031b593eacb2088e2
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-28 18:30:17 -0500

    Product insert, update, delete

--- backend/src/main/java/com/sergiorosa/ecatalog/services/ProductService.java
+++ backend/src/main/java/com/sergiorosa/ecatalog/services/ProductService.java
@@ -1,10 +1,9 @@
 package com.sergiorosa.ecatalog.services;
 
 
-
 import java.util.Optional;
 
-import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -14,12 +13,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sergiorosa.ecatalog.dto.CategoryDTO;
 import com.sergiorosa.ecatalog.dto.ProductDTO;
+import com.sergiorosa.ecatalog.entities.Category;
 import com.sergiorosa.ecatalog.entities.Product;
+import com.sergiorosa.ecatalog.repositories.CategoryRepository;
 import com.sergiorosa.ecatalog.repositories.ProductRepository;
 import com.sergiorosa.ecatalog.services.exceptios.DatabaseException;
 import com.sergiorosa.ecatalog.services.exceptios.ResourceNotFoundException;
-
 
 @Service
 public class ProductService {
@@ -27,69 +28,74 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-		Page<Product> list =  repository.findAll(pageRequest);
-		return list.map(x -> new ProductDTO(x));
-		
+	public Page<ProductDTO> findAllPaged(PageRequest pageResquest){
+			Page<Product> list =  repository.findAll(pageResquest);
+			
+			//	Lambda Expression
+			return list.map(x -> new ProductDTO(x));
 	}
 
-	
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-//		404 error
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ProductDTO(entity, entity.getCategories());
 	}
 
-//	INSERT
 	
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-//		entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
+
 
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
-		Product entity = repository.getOne(id);
-//		entity.setName(dto.getName());
-		entity = repository.save(entity);
-		return new ProductDTO(entity);
-	}
-		catch(EntityExistsException e) {
-			throw new ResourceNotFoundException("Id not found " + id);
+			Product entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
 		}
 	}
-
 
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
 		}
-		catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id not found " + id);
-		}
-		catch (DataIntegrityViolationException e) {
+		catch(DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
 		}
 		
 	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
