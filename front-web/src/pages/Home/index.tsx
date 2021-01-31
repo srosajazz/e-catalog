7b6fb021eceef6736a7a8181d94490b3aa731179
Sergio Rosa <scarlosjazz@gmail.com>
2021-01-31 09:12:20 -0500

    Add Home page

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,22 @@
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+
+import './styles.scss';
+
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="home-container">
+    <div className="row home-content">
+      <div className="col-6">
+        <h1 className="text-title">Find the best product on our catalog </h1>
+
+        <p className="text-subtitle">
+          We will help you find the best <br />
+          products available on the market.
+        </p>
+      </div>
+      <div className="col-6">
+        <MainImage className="main-image" />
+      </div>
+    </div>
   </div>
 );
 
