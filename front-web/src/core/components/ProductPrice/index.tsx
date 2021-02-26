27977dd2119162e312158c9cbe6d6dc6808e9456
Sergio Rosa <scarlosjazz@gmail.com>
2021-02-26 07:58:39 -0500

    Formatting product prices

--- front-web/src/core/components/ProductPrice/index.tsx
+++ front-web/src/core/components/ProductPrice/index.tsx
@@ -4,10 +4,16 @@ type Props = {
   price: number;
 };
 
+const formatPrice = (price: number) => {
+  return new Intl.NumberFormat('en-IN', { minimumFractionDigits: 2 }).format(
+    price
+  );
+};
+
 const ProductPrice = ({ price }: Props) => (
   <div className="product-price-container">
     <span className="product-currency">U$</span>
-    <h3 className="product-price">{price}</h3>
+    <h3 className="product-price">{formatPrice(price)}</h3>
   </div>
 );
 
