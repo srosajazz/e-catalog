cf8a26b407491237b577d9e0b75d90a422cb72be
Sergio Rosa <scarlosjazz@gmail.com>
2021-03-21 07:40:37 -0400

    Creating base forms structure in Admin PART 1

--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import List from './List';
 import './styles.scss';
 
 const Products = () => {
   return (
     <div>
-      <Link to="/admin/products" className="mr-5">
-        List Products
-      </Link>
-      <Link to="/admin/products/create" className="mr-5">
-        CreateProducts
-      </Link>
-      <Link to="/admin/products/10" className="mr-5">
-        Edit Products
-      </Link>
       <Switch>
         <Route path="/admin/products" exact>
-          <h1>Produts List</h1>
+          <List />
         </Route>
         <Route path="/admin/products/create">
           <h1>Create new Product</h1>
