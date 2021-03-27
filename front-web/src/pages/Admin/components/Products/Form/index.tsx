64021d9676247542c8c1f2a945e75bda4db99b1a
Sergio Rosa <scarlosjazz@gmail.com>
2021-03-27 12:21:04 -0400

    Creating base forms structure in Admin PART 1

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -3,61 +3,67 @@ import { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+type FormState = {
+  name: string;
+  price: string;
+  category: string;
+};
+
 const Form = () => {
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
-  const [category, setCategory] = useState('Audition');
-
-  const handleOnChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
-
-  const handleOnChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPrice(event.target.value);
-  };
-
-  const handleOnChangeCategory = (
-    event: React.ChangeEvent<HTMLSelectElement>
+  const [formData, setFormData] = useState<FormState>({
+    name: 'Berklee',
+    price: '',
+    category: '',
+  });
+  const handleOnChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
-    setCategory(event.target.value);
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData((data) => ({ ...data, [name]: value }));
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(formData);
+  };
   return (
-    <BaseForm title="register one product">
-      <h1 className="my-5">
-        Name: {name} <br />
-        Price: {price} <br />
-        Categorie: {category} <br />
-      </h1>
-      <div className="row">
-        <div className="col-6">
-          <input
-            value={name}
-            type="text"
-            className="form-control mb-5"
-            onChange={handleOnChangeName}
-            placeholder="Product Name"
-          />
-          <select
-            value={category}
-            className="form-control mb-5"
-            onChange={handleOnChangeCategory}
-          >
-            <option value="bass">Bass</option>
-            <option value="guitar">Guitar</option>
-            <option value="voice">Voice</option>
-            <option value="woodwind">Woodwind</option>
-          </select>
-          <input
-            value={price}
-            type="text"
-            className="form-control"
-            onChange={handleOnChangePrice}
-            placeholder="Price"
-          />
+    <form onSubmit={handleSubmit}>
+      <BaseForm title="register one product">
+        <div className="row">
+          <div className="col-6">
+            <input
+              value={formData.name}
+              name="name"
+              type="text"
+              className="form-control mb-5"
+              onChange={handleOnChange}
+              placeholder="Product Name"
+            />
+            <select
+              value={formData.category}
+              className="form-control mb-5"
+              onChange={handleOnChange}
+              name="category"
+            >
+              <option value="bass">Bass</option>
+              <option value="guitar">Guitar</option>
+              <option value="voice">Voice</option>
+              <option value="woodwind">Woodwind</option>
+            </select>
+            <input
+              value={formData.price}
+              name="price"
+              type="text"
+              className="form-control"
+              onChange={handleOnChange}
+              placeholder="Price"
+            />
+          </div>
         </div>
-      </div>
-    </BaseForm>
+      </BaseForm>
+    </form>
   );
 };
 
